0a594e85a1c426a0048b6afc2adb477da18709ab
osezelej <2osezelejoseph@gmail.com>
2024-02-15 16:09:39 +0100

    admin 30% remaining

--- src/pages/admin/home.js
+++ src/pages/admin/home.js
@@ -3,9 +3,6 @@ import trending2 from '../../assets/trending2.png';
 import trending3 from '../../assets/trending3.png';
 import image1 from '../../assets/image1.png';
 import image2 from '../../assets/image2.png';
-import {ReactComponent as Profile} from '../../assets/Profile 1.svg';
-import {ReactComponent as Home} from '../../assets/Home3.svg';
-import {ReactComponent as Category} from '../../assets/Category.svg';
 import { useState, useEffect } from 'react';
 import { SearchRounded } from '@mui/icons-material';
 import '../../css/admin.css';
@@ -14,6 +11,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { profileThunk } from '../../store/user';
 import { ClipLoader } from 'react-spinners';
+import axios from 'axios';
+import BACKEND_URL, {LOCALSTORAGEACCESSTOKENKEY} from '../../config';
 
 export function AdminHome(){
     const profileState = useSelector(state=>state.user.profile)
@@ -26,7 +25,60 @@ export function AdminHome(){
     const userid = searchParams.get('q')
 
     const [artistProfileLoading, setArtistProfileLoading] = useState(true)
+     const [loadingdata, setLoadingData] = useState(false);
 
+    async function getPublishedData( category, page ){
+        setLoadingData(true);
+        let data;
+        if(!navigator.onLine){
+            setErrorMessage({
+                title:'Network Error',
+                content:'you do not have an ',
+                command:['okay'],
+                type:'warning'
+            });
+            return setOpenModal(true)
+        }
+        let accessToken = localStorage.getItem(LOCALSTORAGEACCESSTOKENKEY);
+        await axios.get(BACKEND_URL + `/${category}/all` + `?page=${page}&pageSize=${10}`, {
+            headers:{
+                Authorization:`Bearer ${accessToken}`
+            }
+        }).then((value)=>{
+            console.log(value.data.data)
+            data = value.data.data;
+        }).catch((err)=>{
+            console.log(err)
+            setErrorMessage({
+                title:'Error',
+                content:'An error occured ',
+                command:['okay'],
+                type:'warning'
+            });
+            setOpenModal(true)
+        }).finally(()=>{
+            setLoadingData(false)
+        })
+        if(data){
+            return data;
+        }
+        else{
+            return false
+        }
+    }
+    async function handleDeleteData(category, itemId){
+        const accessToken = localStorage.getItem(LOCALSTORAGEACCESSTOKENKEY);
+        await axios.delete(BACKEND_URL + `/${category}/${itemId}`, {
+            headers:{
+                Authorization:`Bearer ${accessToken}`
+            }
+        }).then((value)=>{
+            console.log(value.data);
+        }).catch((err)=>{
+            console.log(err)
+        });
+        
+    }
 
     const [errorMessage, setErrorMessage] = useState({
         title:"",
@@ -38,29 +90,13 @@ export function AdminHome(){
     })
 
     const [trendingData, setTrandingData] = useState([1,2,3,4,54,5,6,7,3,3]);
-    const iconTextArray =[
-        {
-            icon:Home,
-            text:"Home",
-            link:"/market/home"
-        },
-        {
-            icon:Category,
-            text:"Shop",
-            link:"/market/shop"
-        },
-        {
-            icon:Category,
-            text:"Collection",
-            link:"/collection"
-        },
-        {
-            icon:Profile,
-            text:"Community"
-        },
-    ]
-
-    let imagesArray = [image1, image2,image1, image2,image1, image2,image1, image2,image1, image2,image1, image2,image1, image2,image1, image2,image1, image2,image1, image2,image1, image2];
+    let [imagesArray , setImageArray]= useState([]);
+    const [albumContent, setAlbumContent] = useState([]);
+    const [storyContent, setStoriesContent] = useState([]);
+    const [categoryData, setCategoryData] = useState({
+        category:'artwork',
+        page:1
+    });
     const imageArray = [image1, image2, trending1, trending2, trending3]
     let [chosenContent, SetChosenContent] = useState({
         store:true,
@@ -92,6 +128,36 @@ export function AdminHome(){
 
     }, [profileState])
 
+
+    useEffect(()=>{
+        if(chosenContent.store){
+            
+        getPublishedData('artwork', categoryData.page).then((value)=>{
+            if (value){
+                console.log(value.data)
+                setImageArray(value.data)
+            }
+        })
+
+        }else if (chosenContent.album){
+
+            getPublishedData('album', categoryData.page).then((value)=>{
+                if (value){
+                    console.log(value.data)
+                    setAlbumContent(value.data)
+                }
+            })
+
+        }else{
+            
+            getPublishedData('story', categoryData.page).then((value)=>{
+                if (value){
+                    console.log(value.blogs)
+                    setStoriesContent(value.blogs)
+                }
+            })
+        }
+    }, [categoryData.page, chosenContent]);
     
 
 
@@ -127,20 +193,33 @@ export function AdminHome(){
                 <h4>Edit collections</h4>
                 <p className="active">see all</p>
             </div>
+            
+            <div >
+            { loadingdata ? <div style={{
+                        height:'100%',
+                        width:'100%',
+                        padding:'50px 0',
+                        display:'flex',
+                        justifyContent:'center'
+                        
+                    }}>
+                        <ClipLoader color="#BE774C" size={30} />
+                    </div>:
             <div className="home-image-list-container">
-            {
-                trendingData.map((value, index)=> <div className="home-image-item-container" key={index}>
+            
+           {trendingData.map((value, index)=> <div className="home-image-item-container" key={index}>
                     <div>
                         <img alt="artwork"  src={trending1}/>
                     </div>
                     <div>
-                        <img alt="artwork"  src={trending2}/>
+                        <img alt=""  src={trending2}/>
                     </div>
                     <div>   
-                        <img alt="artwork"  src={trending3}/>
+                        <img alt=""  src={trending3}/>
                     </div>
                 </div>)
-            }
+            } 
+            </div>}
             </div>
 
             <div className="home-explore-main-section">
@@ -185,34 +264,65 @@ export function AdminHome(){
                         }}/>
                         <input type='text' placeholder='Search'/>
                     </div>
-                {chosenContent.store && <div className="home-photo-mainbody" style={{rowGap:25}}>
+                    { loadingdata && <div style={{
+                        height:'100vh',
+                        width:'98vw',
+                        padding:'50px 0',
+                        display:'flex',
+                        justifyContent:'center'
+                        
+                    }}>
+                        <ClipLoader color="#BE774C" size={30} />
+                    </div>
+                }
+                {(chosenContent.store && ! loadingdata) && <div className="home-photo-mainbody" style={{rowGap:25}}>
                     {imagesArray.map((value, index)=> <div className='shop-body-content-item-container' key={index}>
-                            <div className='image-section'>
-                                <img src={value} alt='photo'/>
+                            <div className='image-section' style={{
+                                    width:'5%',
+                                    height:'60%'
+                                }}>
+                                <img src={value.imageUris[0]} alt='' style={{
+                                    objectFit:'contain',
+                                    height:'100%'
+                                }}/>
                             </div>
                             <div className='item-name-text-price-container'>
                                 <div className='item-name-text-container'>
                                     <p className='title'>
-                                        Walls of Kano
+                                       {value.title}
                                     </p>
                                     <p className='text'>
-                                        The Ancient kano city walls
+                                        {value.description.substring(0, 25)}
                                     </p>
                                 </div>
-                                <p className='price'>$112</p>
+                                <p className='price'>${value.price}</p>
                             </div>
                             <div className='share-shopnow-button-container'>
                                 
-                                <button className='shopnow-button' style={{width:'100%'}}>Delete Now </button>
+                                <button 
+                                    className='shopnow-button' 
+                                    style={{width:'100%'}} 
+                                    onClick={()=>{
+                                        handleDeleteData('artwork', value.id).then((value)=>{
+                                            getPublishedData('artwork', 1).then((value)=>{
+                                                if (value){
+                                                    console.log(value.data)
+                                                    setImageArray(value.data)
+                                                }
+                                            })
+                                        })
+                                }}>
+                                Delete Now 
+                                </button>
                             </div>
 
                     </div>)
                     }
                 </div>
                 }
-
-                {chosenContent.album && <div className="home-photo-mainbody" style={{rowGap:25}}>
-                    {imagesArray.map((value, index)=> <div className='shop-body-content-item-container' key={index}>
+               
+                {(chosenContent.album && ! loadingdata)&& <div className="home-photo-mainbody" style={{rowGap:25}}>
+                    {albumContent.map((value, index)=> <div className='shop-body-content-item-container' key={index}>
                             <div className='image-section'>
                                 <img src={value} alt='photo'/>
                             </div>
@@ -235,8 +345,8 @@ export function AdminHome(){
                     </div>)
                     }
                 </div>}
-                {chosenContent.stories &&  <div className='Stories-body-container'>
-                            {imageArray.map((value)=><div>
+                {(chosenContent.stories && ! loadingdata) &&  <div className='Stories-body-container'>
+                            {storyContent.map((value)=><div>
                                 <div className='stories-item-container'>
 
                                     <div className='text-container'>
